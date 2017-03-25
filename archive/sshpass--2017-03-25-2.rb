9abb8d906be998ad819a5cfe65f087a1cd9ffc01
Josh Rodd <josh@rodd.us>
2017-03-25 16:41:49 -0400

    Updated archive .zip for 1.06-brew-2017-03-25-2

--- archive/sshpass--2017-03-25-2.rb
+++ archive/sshpass--2017-03-25-2.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Sshpass < Formula
   url 'https://github.com/JoshRodd/sshpass/archive/sshpass-1.06-brew-2017-03-25-2.zip'
   homepage 'https://github.com/JoshRodd/sshpass'
-  sha256 '260e9a175b1c23c9e9bad34e5142ef8c747da31e50c0e493cfe8b76d463039e6'
+  sha256 '400842c420885574dd7fefcd4b4fe1c9435e9e3c75e383195a340325ef71b628'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
